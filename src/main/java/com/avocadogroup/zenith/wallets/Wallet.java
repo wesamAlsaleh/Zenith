c3a05fd77eecd96994f3a2668e6d52822b9f1bca
wesamAlsaleh <wesammuneer7@gmail.com>
2026-04-14 19:27:22 +0300

    feat: add wallet table and create the entity

---
+++ src/main/java/com/avocadogroup/zenith/wallets/Wallet.java
@@ -0,0 +1,42 @@
+package com.avocadogroup.zenith.wallets;
+
+import com.avocadogroup.zenith.users.User;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.time.Instant;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "wallets")
+public class Wallet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "balance", nullable = false)
+    private Double balance;
+
+    @Size(max = 255)
+    @Column(name = "currency", nullable = false)
+    private String currency;
+
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false)
+    private Instant createdAt;
+}
